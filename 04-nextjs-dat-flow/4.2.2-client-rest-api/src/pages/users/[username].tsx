2bfebdfc18517e6a5aa51275a0e021f1a88a8afe
이영범 <youngbeomrhee@zigbang.com>
2023-10-09 02:03:07 +0900

    feat: 4.2.2 클라이언트에서 REST API 사용하기 - 초기구현 (리팩토링 전)

---
+++ 04-nextjs-dat-flow/4.2.2-client-rest-api/src/pages/users/[username].tsx
@@ -0,0 +1,76 @@
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+export async function getServerSideProps({ query }) {
+  const { username } = query
+
+  return {
+    props: {
+      username,
+      authorization: process.env.API_TOKEN,
+    },
+  }
+}
+
+export function UserData({ user }) {
+  return (
+    <div style={{ display: 'flex' }}>
+      <img
+        src={user.profile_picture}
+        alt={user.username}
+        width={150}
+        height={150}
+      />
+      <div>
+        <div>
+          <b>Username:</b> {user.username}
+        </div>
+        <div>
+          <b>Full name:</b> {user.first_name} {user.last_name}
+        </div>
+        <div>
+          <b>Email:</b> {user.email}
+        </div>
+        <div>
+          <b>Company:</b> {user.company}
+        </div>
+        <div>
+          <b>Job title:</b> {user.job_title}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function UserPage({ username, authorization }) {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await fetch(`/api/04/users/${username}`, {
+        headers: { authorization },
+      })
+      const users = await req.json()
+
+      setLoading(false)
+      setData(users)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <div>
+        <Link href="/" passHref>
+          Back to home
+        </Link>
+      </div>
+      <hr />
+      {loading && <div>Loading user data...</div>}
+      {data && <UserData user={data} />}
+    </div>
+  )
+}
+
+export default UserPage
