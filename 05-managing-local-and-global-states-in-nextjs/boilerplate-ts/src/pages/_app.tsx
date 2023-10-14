a90b8f81dcc6970de70d04085e6796fa537e24df
이영범 <youngbeomrhee@zigbang.com>
2023-10-14 18:06:03 +0900

    feat: boilerplate 추가

---
+++ 05-managing-local-and-global-states-in-nextjs/boilerplate-ts/src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import Navbar from '@/components/Navbar'
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Navbar />
+      <div className="w-9/12 m-auto pt-10">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
+}
+
+export default MyApp
